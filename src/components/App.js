f2830b78e7dbbfa1d8e67b8a0d21dd62c315ec62
Alejandro Sanchez <alejandroszg@gmail.com>
2023-08-08 17:22:04 -0400

    final project lesson

---
+++ src/components/App.js
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import Logo from "./Logo";
+import Stats from "./Stats";
+import Form from "./Form";
+import PackingList from "./PackingList";
+
+export default function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItem(newItem) {
+    setItems((prevItems) => [...prevItems, newItem]);
+  }
+
+  function handleDeleteItem(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+
+  function handleToggleItem(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
+
+  function handleClearList() {
+    const confirmed = window.confirm(
+      "Are you sure you want to detele all items?"
+    );
+    if (confirmed) setItems([]);
+  }
+
+  return (
+    <div className="app">
+      <Logo />
+      <Form onAddItems={handleAddItem} />
+      <PackingList
+        items={items}
+        onDeleteItem={handleDeleteItem}
+        onToggleItem={handleToggleItem}
+        onClearList={handleClearList}
+      />
+      <Stats items={items} />
+    </div>
+  );
+}
